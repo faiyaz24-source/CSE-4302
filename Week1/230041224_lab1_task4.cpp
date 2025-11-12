171fa858d61ff45c6fd39931a52a42dab0b9597b
faiyaz24-source <faiyaz24@iut-dhaka.edu>
2025-11-12 10:01:47 +0600

    Create 230041224_lab1_task4.cpp

---
+++ Week1/230041224_lab1_task4.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter value of n: ";
+    cin >> n;
+    int p=2*n+2;
+    cout << "Formula: C" << n << "H" << p;
+    return 0;
+}
