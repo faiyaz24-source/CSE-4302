0fc6eebacbc2a7f1fb13057af78030f81332d022
faiyaz24-source <faiyaz24@iut-dhaka.edu>
2025-11-12 10:00:38 +0600

    Create 230041224_lab1_task3.cpp

---
+++ Week1/230041224_lab1_task3.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double pi=3.141593;
+    double k;
+    cout << "Enter value in degrees:";
+    cin >> k;
+    double ans=(pi*k)/180.00;
+    cout<< "Value in radians: " << ans << endl;
+    return 0;
+}
