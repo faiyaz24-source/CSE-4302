4682c62a8b8b70bd96a41aa759b0598701b17f26
faiyaz24-source <faiyaz24@iut-dhaka.edu>
2025-11-12 09:56:47 +0600

    Update and rename task-1 to 230041224_lab1_task-1.cpp

---
+++ Week1/230041224_lab1_task-1.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout << "I  I        I  I" << endl;
+    cout << "I  I\\  /\\  /I  I" << endl;
+    cout << "I__I \\/  \\/ I__I" << endl;
+    return 0;
+}
+
